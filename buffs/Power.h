161581cdc3429795955f68bc8671ebe800681025
Gordon <gordonliu1000@gmail.com>
2025-12-15 00:50:04 +0800

    合併衝突

--- buffs/Power.h
+++ buffs/Power.h
@@ -9,15 +9,9 @@ public:
     ~Power(){};
     void effect();
     void clear_effect();
-<<<<<<< HEAD
 private:
-    bool used = false;
+    float factor = 2.0;
     double init_attack;
-=======
-// private:
-//     double init_attack;
-//     bool init_attack_saved = false;
->>>>>>> a6edc0fbf093f1443f43641e6ee3140de2a32266
 };
 
 #endif
